0d9be26c7d982534ecace7358996517d815aec76
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-05-30 11:28:15 -0700

    add version to lnd dockerfile, add btcd, add fund script to package json

--- scripts/fund.sh
+++ scripts/fund.sh
@@ -12,9 +12,4 @@ MINING_ADDRESS="$answer" docker-compose up -d btcd
 GENERATE_CMD='btcctl --simnet --rpcuser="$RPC_USER" --rpcpass="$RPC_PASS" --rpccert="$RPC_CERT" generate 400'
 docker-compose exec -T btcd /bin/sh -c "$GENERATE_CMD"
 
-echo -n "Give me the public key of your relayer"
-
-sleep 5
-
-CREATE_CHANNEL="Engine.createChannel('docker.for.mac.host.internal:10111', '03224d527c38b81f5f31e07194d048b4d12b86c6e3c947da46b68dca176d50f1f4', '20000').then(console.log).catch(console.error)"
-echo "$CREATE_CHANNEL" | docker-compose run lnd_repl
+echo "Engine.getTotalBalance().then(console.log)" | docker-compose run lnd_repl
