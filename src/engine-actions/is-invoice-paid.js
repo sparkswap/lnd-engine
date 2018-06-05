8965f45bb62f054e4f4745f5ce46b0ae53ab0c20
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-06-05 14:55:31 -0700

    add documentation

--- src/engine-actions/is-invoice-paid.js
+++ src/engine-actions/is-invoice-paid.js
@@ -1,5 +1,11 @@
 const { lookupInvoice } = require('../lnd-actions')
 
+/**
+ * Looks up whether or not an invoice has been paid
+ *
+ * @param {String} rHash - invoice request hash
+ * @returns {Boolean} true if the invoice is settled, false if not
+ */
 async function isInvoicePaid (invoiceHash) {
   const { settled } = await lookupInvoice(invoiceHash, { client: this.client })
   return settled
