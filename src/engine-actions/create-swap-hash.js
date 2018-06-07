682e546ec17385d7f02f86c6236f8244694d63b8
Trey Griffith <trey.griffith@gmail.com>
2018-06-06 18:00:09 -0700

    add createSwapHash function

---
+++ src/engine-actions/create-swap-hash.js
@@ -0,0 +1,19 @@
+const { addInvoice } = require('../lnd-actions')
+
+/**
+ * Creates a swap hash
+ *
+ * @param {String} orderId order ID for the swap hash
+ * @param {Number} value
+ * @returns {String} rHash hash of invoice from lnd
+ */
+async function createSwapHash (orderId, value) {
+  // default expiry for swap hashes is 3600 seconds - 1 hour.
+  // TODO: should we change that?
+  const expiry = '3600'
+  const memo = `kinesis:${orderId}`
+  const { rHash } = await addInvoice(memo, expiry, value, { client: this.client })
+  return rHash
+}
+
+module.exports = createSwapHash
---
+++ src/engine-actions/create-swap-hash.spec.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const { expect, rewire, sinon } = require('test/test-helper')
+
+const createSwapHash = rewire(path.resolve(__dirname, 'create-swap-hash'))
+
+describe('createSwapHash', () => {
+  let orderId
+  let expiry
+  let value
+  let addInvoiceStub
+  let clientStub
+  let invoiceResponse
+  let rHash
+  let res
+
+  beforeEach(() => {
+    orderId = '928uq9afds8as9df_fasdfj'
+    value = '100'
+    rHash = Buffer.from('1234')
+    invoiceResponse = { rHash }
+    addInvoiceStub = sinon.stub().resolves(invoiceResponse)
+    clientStub = sinon.stub()
+
+    createSwapHash.__set__('addInvoice', addInvoiceStub)
+    createSwapHash.__set__('client', clientStub)
+  })
+
+  beforeEach(async () => {
+    res = await createSwapHash(orderId, value)
+  })
+
+  it('adds an invoice through lnd', () => {
+    expect(addInvoiceStub).to.have.been.calledWith(`kinesis:${orderId}`, '3600', value, sinon.match({ client: clientStub }))
+  })
+
+  it('returns an invoice hash hash', () => {
+    expect(res).to.be.eql(rHash)
+  })
+})
