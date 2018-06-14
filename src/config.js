3441a2a09b2f2c36a964d9b161d08995ef22064a
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-06-14 14:39:16 -0700

    updated docs and added supported symbols

--- src/config.js
+++ src/config.js
@@ -1,18 +1,29 @@
 /**
  * @constant
- * @type {String}
+ * @type {Number}
  * @default
  */
 const LTC_FEE_PER_KW = 7667
 
 /**
  * @constant
- * @type {String}
+ * @type {Number}
  * @default
  */
 const BTC_FEE_PER_KW = 6667
 
+/**
+ * @constant
+ * @type {Array<key, string>}
+ * @default
+ */
+const SUPPORTED_SYMBOLS = Object.freeze({
+  BTC: 'BTC',
+  LTC: 'LTC'
+})
+
 module.exports = {
   LTC_FEE_PER_KW,
-  BTC_FEE_PER_KW
+  BTC_FEE_PER_KW,
+  SUPPORTED_SYMBOLS
 }
