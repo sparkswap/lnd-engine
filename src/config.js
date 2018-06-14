2a7ccd0037b0647f735c05158e945c354d3e0ba1
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-06-14 14:34:48 -0700

    added fee config to lnd-engine

---
+++ src/config.js
@@ -0,0 +1,18 @@
+/**
+ * @constant
+ * @type {String}
+ * @default
+ */
+const LTC_FEE_PER_KW = 7667
+
+/**
+ * @constant
+ * @type {String}
+ * @default
+ */
+const BTC_FEE_PER_KW = 6667
+
+module.exports = {
+  LTC_FEE_PER_KW,
+  BTC_FEE_PER_KW
+}
