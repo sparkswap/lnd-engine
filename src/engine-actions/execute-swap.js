2a9634051ceed340472595b44e85abb6e54d7daa
Trey Griffith <trey.griffith@gmail.com>
2018-06-14 17:23:12 -0700

    standard

--- src/engine-actions/execute-swap.js
+++ src/engine-actions/execute-swap.js
@@ -19,9 +19,9 @@ async function executeSwap (counterpartyPubKey, swapHash, inbound, outbound) {
   this.logger.info(`Executing swap for ${swapHash} with ${counterpartyPubKey}`, { inbound, outbound })
 
   // get all routes
-  const [ graph, { identityPubkey } = await Promise.all([
+  const [ graph, { identityPubkey } ] = await Promise.all([
     describeGraph({ client: this.client }),
-    getInfo({ client: this.client})
+    getInfo({ client: this.client })
   ])
 
   console.log('graph')
@@ -32,12 +32,12 @@ async function executeSwap (counterpartyPubKey, swapHash, inbound, outbound) {
   console.log('outboundPath')
   console.log(outboundPath)
 
-  const inboundPath = findPaths(graph.edges, counterpartyPubkey, identityPubKey, inbound.symbol, inbound.amount)
+  const inboundPath = findPaths(graph.edges, counterpartyPubKey, identityPubkey, inbound.symbol, inbound.amount)
   console.log('inboundPath')
   console.log(inboundPath)
 
-  if(!outboundPath || !inboundPath) {
-    throw new Error(`Can't find a route between ${identityPubkey} and ${counterpartyPubkey} to swap ${outbound.amount} ${outbound.symbol} for ${inbound.amount} ${inbound.symbol}`)
+  if (!outboundPath || !inboundPath) {
+    throw new Error(`Can't find a route between ${identityPubkey} and ${counterpartyPubKey} to swap ${outbound.amount} ${outbound.symbol} for ${inbound.amount} ${inbound.symbol}`)
   }
 
   // construct a valid route
@@ -46,16 +46,16 @@ async function executeSwap (counterpartyPubKey, swapHash, inbound, outbound) {
 }
 
 // naive path finding
-async function findPaths(edges, fromPubKey, toPubKey, symbol, amount, visited = []) {
+async function findPaths (edges, fromPubKey, toPubKey, symbol, amount, visited = []) {
   const candidates = findOutboundChannels(edges, fromPubKey, symbol, amount, visited)
 
   const endOfPath = candidates.find(({ node1Pub, node2Pub, channelId }) => {
-    if((node1Pub === fromPubKey && node2Pub === toPubKey) || (node1Pub === toPubKey && node2Pub === fromPubKey)) {
+    if ((node1Pub === fromPubKey && node2Pub === toPubKey) || (node1Pub === toPubKey && node2Pub === fromPubKey)) {
       return true
     }
   })
 
-  if(endOfPath) return [ endOfPath ]
+  if (endOfPath) return [ endOfPath ]
 
   const paths = candidates.map(candidate => {
     const { node1Pub, node2Pub } = candidate
@@ -74,35 +74,35 @@ async function findPaths(edges, fromPubKey, toPubKey, symbol, amount, visited = 
   return paths[0]
 }
 
-async function findOutboundChannels(edges, fromPubKey, symbol, amount, visited = []) {
+async function findOutboundChannels (edges, fromPubKey, symbol, amount, visited = []) {
   return edges.filter(({ node1Pub, node2Pub, capacity, node1Policy, node2Policy, channelId }) => {
     // don't retrace
-    if(visited.includes(channelId)) {
+    if (visited.includes(channelId)) {
       return false
     }
 
     // neither node is ours
-    if(node1Pub !== fromPubKey && node2Pub !== fromPubKey) {
+    if (node1Pub !== fromPubKey && node2Pub !== fromPubKey) {
       return false
     }
 
     // not the right symbol
     let channelSymbol
-    if(node1Policy.feeRateMilliMsat === LTC_FEE_PER_KW && node2Policy.feeRateMilliMsat === LTC_FEE_PER_KW) {
+    if (node1Policy.feeRateMilliMsat === LTC_FEE_PER_KW && node2Policy.feeRateMilliMsat === LTC_FEE_PER_KW) {
       channelSymbol = 'LTC'
-    } else if(node1Policy.feeRateMilliMsat === BTC_FEE_PER_KW && node2Policy.feeRateMilliMsat === BTC_FEE_PER_KW) {
+    } else if (node1Policy.feeRateMilliMsat === BTC_FEE_PER_KW && node2Policy.feeRateMilliMsat === BTC_FEE_PER_KW) {
       channelSymbol = 'BTC'
     } else {
       throw new Error('Channel is on unidentified blockchain')
     }
 
     // not for our symbol
-    if(channelSymbol !== symbol) {
+    if (channelSymbol !== symbol) {
       return false
     }
 
     // not enough capacity
-    if(Big(capacity).lt(amount)) {
+    if (Big(capacity).lt(amount)) {
       return false
     }
 
