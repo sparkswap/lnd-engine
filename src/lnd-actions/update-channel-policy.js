0b1cacaf055390716dc51ee633640d24ed4a7fe0
Trey Griffith <trey.griffith@gmail.com>
2018-06-16 12:44:31 -0700

    allow all of the parameters to be set for channel policies

--- src/lnd-actions/update-channel-policy.js
+++ src/lnd-actions/update-channel-policy.js
@@ -11,12 +11,15 @@ const DEFAULT_TIMELOCK_DELTA = 9
  * @param {String} channelPoint.fundingTxidBytes
  * @param {String} channelPoint.fundingTxidStr
  * @param {String} channelPoint.outputIndex
- * @param {String} baseFeeMsat Base fee in millisatoshis expressed as an int64 string
+ * @param {Object} policy
+ * @param {String} policy.baseFeeMsat Base fee in millisatoshis expressed as an int64 string
+ * @param {Number} policy.feeRate Fee rate in millisatoshis per million satoshis
+ * @param {Number} policy.timeLockDelta Time lock delta required to pass through the channel
  * @returns {Promise}
  */
-async function updateChannelPolicy (chanPoint, baseFeeMsat, { client }) {
+async function updateChannelPolicy (chanPoint, { baseFeeMsat = '0', feeRate = 0, timeLockDelta = DEFAULT_TIMELOCK_DELTA }, { client }) {
   return new Promise((resolve, reject) =>
-    client.updateChannelPolicy({ chanPoint, baseFeeMsat, feeRate: 1, timeLockDelta: DEFAULT_TIMELOCK_DELTA }, (err, res) => {
+    client.updateChannelPolicy({ chanPoint, baseFeeMsat, feeRate, timeLockDelta }, (err, res) => {
       if (err) return reject(err)
       return resolve(res)
     })
