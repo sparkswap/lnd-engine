723d85d38bfb6773e3512588aef5c71a0ad312d0
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-06-29 10:47:56 -0700

    return the channels so we can debug

--- src/engine-actions/get-total-channel-balance.js
+++ src/engine-actions/get-total-channel-balance.js
@@ -10,37 +10,13 @@ const {
  * @return {Array<Symbol, Big>}
  */
 async function getTotalChannelBalance (remotePubKey) {
-  const balances = {}
+  // const balances = {}
 
   const { channels = [] } = await listChannels({ client: this.client })
 
   const { pendingOpenChannels = [] } = await pendingChannels({ client: this.client })
 
-  console.log('channels are', channels)
-  console.log('pendingOpenChannels are', pendingOpenChannels)
-
-  if (channels.length === 0 || pendingOpenChannels.length === 0) {
-    this.logger.debug('getTotalChannelBalance: No channels exist')
-    return balances
-  }
-
-  console.log('made it here')
-  const activeChannelsForPubkey = channels.filter(channel => channel.remotePubkey === remotePubKey)
-  console.log('activeChannelsForPubkey', activeChannelsForPubkey)
-  const amountInActiveChannels = activeChannelsForPubkey.reduce((acc, chan) => (acc += chan.remoteBalance))
-  console.log('amountInActiveChannels', amountInActiveChannels)
-  balances['active'] = amountInActiveChannels
-  console.log('balances', balances)
-  const pendingChannelsForPubkey = pendingOpenChannels.filter(channel => channel.channel.remoteNodePub === remotePubKey)
-  console.log('pendingChannelsForPubkey', pendingChannelsForPubkey)
-  const amountInPendingChannels = pendingChannelsForPubkey.reduce((acc, chan) => (acc += chan.remoteBalance))
-  console.log(amountInPendingChannels)
-  balances['pending'] = amountInPendingChannels
-  console.log('balances', balances)
-  balances['pending'] = amountInPendingChannels
-  console.log('balances with pending', balances)
-
-  return balances
+  return { channels, pendingOpenChannels }
 }
 
 module.exports = getTotalChannelBalance
