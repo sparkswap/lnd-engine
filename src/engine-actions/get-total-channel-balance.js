3d8f2a60758b52507ef1f3481d4f8bc2965b45b5
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-06-29 10:11:18 -0700

    fix condition and add test logging

--- src/engine-actions/get-total-channel-balance.js
+++ src/engine-actions/get-total-channel-balance.js
@@ -16,15 +16,21 @@ async function getTotalChannelBalance (remotePubKey) {
 
   const { pendingOpenChannels = [] } = await pendingChannels({ client: this.client })
 
-  if (channels.length === 0 || pendingOpenChannels.length === 0) {
+  console.log('channels are', channels)
+  console.log('pendingOpenChannels are', pendingOpenChannels)
+
+  if (channels.length === 0 && pendingOpenChannels.length === 0) {
     this.logger.debug('getTotalChannelBalance: No channels exist')
     return balances
   }
 
+  console.log('made it here')
   balances['active'] = channels.filter(channel => channel.remotePubkey === remotePubKey)
     .reduce((acc, chan) => (acc += chan.remoteBalance))
+  console.log('balances', balances)
   balances['pending'] = pendingOpenChannels.filter(channel => channel.channel.remoteNodePub === remotePubKey)
     .reduce((acc, chan) => (acc += chan.channel.remoteBalance))
+  console.log('balances with pending', balances)
 
   return balances
 }
