e57e15e63a446037a598357f25dd5ddb758f7a9b
Martine Ehrlich <martineehrlich@gmail.com>
2018-07-03 10:26:06 -0700

    move code from relayer back into lnd

--- src/engine-actions/get-total-channel-balance.js
+++ src/engine-actions/get-total-channel-balance.js
@@ -4,19 +4,40 @@ const {
   pendingChannels
 } = require('../lnd-actions')
 
+const { Big } = require('../utils')
+
 /**
  * Get committed balance of all channels
  *
- * @return {Array<Symbol, Big>}
+ * @return {Object.<active, pending>} values are total balances in active and pending channels
  */
 async function getTotalChannelBalance (remotePubKey) {
-  // const balances = {}
-
   const { channels = [] } = await listChannels({ client: this.client })
 
   const { pendingOpenChannels = [] } = await pendingChannels({ client: this.client })
 
-  return { channels, pendingOpenChannels }
+  const balances = {
+    active: new Big(0),
+    pending: new Big(0)
+  }
+
+  if (channels.length === 0 && pendingOpenChannels.length === 0) {
+    throw new Error('getTotalChannelBalance: No channels exist')
+  }
+
+  const activeChannelsForPubkey = channels.filter(channel => channel.remotePubkey === remotePubKey)
+  for (const { remoteBalance } of activeChannelsForPubkey) {
+    const balance = new Big(remoteBalance)
+    balances['active'] = balances['active'].add(balance)
+  }
+
+  const pendingChannelsForPubkey = pendingOpenChannels.filter(channel => channel.channel.remoteNodePub === remotePubKey)
+  for (const { remoteBalance } of pendingChannelsForPubkey) {
+    const balance = new Big(remoteBalance)
+    balances['pending'] = balances['pending'].add(balance)
+  }
+
+  return { activeBalance: balances['active'].toString(), pendingBalance: balances['pending'].toString() }
 }
 
 module.exports = getTotalChannelBalance
