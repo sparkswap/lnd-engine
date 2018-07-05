1d927f854ea0de15b82d52f2177f5706bef0995f
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-05 12:34:40 -0400

    ltcd changes for directory

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -4,6 +4,7 @@ set -e -u
 
 # NOTE: The names specified with `-t` directly map to the service names in
 # the applicable services docker-compose file
-docker build -t kinesis_lnd ./docker/lnd
+docker build -t kinesis_lnd_btc ./docker/lnd --build-arg NETWORK=btc
+docker build -t kinesis_lnd_ltc ./docker/lnd --build-arg NETWORK=ltc
 docker build -t kinesis_ltcd ./docker/ltcd
 docker build -t kinesis_btcd ./docker/btcd
