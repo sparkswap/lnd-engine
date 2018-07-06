42582779f8419417726afce29d120fc703b2ea9d
Martine Ehrlich <martineehrlich@gmail.com>
2018-07-06 13:56:15 -0700

    add specs for no channels being open

--- src/engine-actions/get-total-channel-balance.spec.js
+++ src/engine-actions/get-total-channel-balance.spec.js
@@ -8,23 +8,29 @@ describe('getTotalChannelBalance', () => {
   let listPendingChannelsStub
   let channel
   let channels
-  let pendingChannels
+  let pendingOpenChannels
   let pendingChannel
   let reverts
   let remotePubKey
+  let loggerStub
 
   beforeEach(() => {
     remotePubKey = 'asdf'
-    channel = { remotePubKey, remoteBalance: 500 }
+    channel = { remotePubkey: remotePubKey, remoteBalance: 500 }
     channels = [channel, channel]
     pendingChannel = { channel: { remoteNodePub: remotePubKey, remoteBalance: 400 } }
-    pendingChannels = [pendingChannel]
+    pendingOpenChannels = [pendingChannel]
     listChannelsStub = sinon.stub().returns({ channels })
-    listPendingChannelsStub = sinon.stub().returns({ pendingChannels })
+    listPendingChannelsStub = sinon.stub().returns({ pendingOpenChannels })
+    loggerStub = {
+      debug: sinon.stub(),
+      error: sinon.stub()
+    }
 
     reverts = []
     reverts.push(getTotalChannelBalance.__set__('listChannels', listChannelsStub))
     reverts.push(getTotalChannelBalance.__set__('listPendingChannels', listPendingChannelsStub))
+    reverts.push(getTotalChannelBalance.__set__('logger', loggerStub))
   })
 
   afterEach(() => {
@@ -43,13 +49,24 @@ describe('getTotalChannelBalance', () => {
 
   it('returns the added balance of the active and pending channels', async () => {
     const res = await getTotalChannelBalance(remotePubKey)
-    expect(res).to.eql({activeBalance: '500', pendingBalance: '400'})
+    expect(res).to.eql({activeBalance: '1000', pendingBalance: '400'})
   })
 
   it('filters out channels that do not have the remotePubKey', async () => {
-    const anotherChannel = { remotePubKey: 'differentpubkey', remoteBalance: 500 }
+    const anotherChannel = { remotePubkey: 'differentpubkey', remoteBalance: 500 }
     channels = [channel, anotherChannel]
+    listChannelsStub.returns({ channels })
     const res = await getTotalChannelBalance(remotePubKey)
     expect(res).to.eql({activeBalance: '500', pendingBalance: '400'})
+  })
+
+  it('returns empty values if there are no active or pending channels', async () => {
+    channels = []
+    pendingOpenChannels = []
+    listChannelsStub.returns({ channels })
+    listPendingChannelsStub.returns({ pendingOpenChannels })
+    const res = await getTotalChannelBalance(remotePubKey)
+    expect(res).to.eql({activeBalance: '0', pendingBalance: '0'})
+    expect(loggerStub.debug).to.have.been.calledWith('getTotalChannelBalance: No channels exist')
   })
 })
