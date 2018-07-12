9fcd17f6dc9de93d2d8b04af94d0187b1d639c7b
Danny Paz <dan.pazuchowski@gmail.com>
2018-07-12 15:55:44 -0700

    FIX - GitHub cache issue (#61)
    * add cache date to build scripts to invalidate github cache and pull latest code on image build
    
    * add comment about why CACHE_DATE
    
    * fix cache arg
    
    * changed cache_date to commit_sha
    
    * use commit hash for git clone

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -2,9 +2,14 @@
 
 set -e -u
 
+# We add a COMMIT_SHA argument to the lnd dockerfile to trigger cache-invalidation
+# when get git clone the kinesis-exchange/lnd repo. Without it, docker would continue
+# to cache old code and we would never receive updates from the fork.
+COMMIT_SHA=`git ls-remote git://github.com/kinesis-exchange/lnd | grep refs/heads/k#epic/cross-chain-preimage | cut -f 1`
+
 # NOTE: The names specified with `-t` directly map to the service names in
 # the applicable services docker-compose file
-docker build -t kinesis_lnd_btc ./docker/lnd --build-arg NETWORK=btc
-docker build -t kinesis_lnd_ltc ./docker/lnd --build-arg NETWORK=ltc
+docker build -t kinesis_lnd_btc ./docker/lnd --build-arg NETWORK=btc --build-arg COMMIT_SHA=$COMMIT_SHA
+docker build -t kinesis_lnd_ltc ./docker/lnd --build-arg NETWORK=ltc --build-arg COMMIT_SHA=$COMMIT_SHA
 docker build -t kinesis_ltcd ./docker/ltcd
 docker build -t kinesis_btcd ./docker/btcd
