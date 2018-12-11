28242b593eeedfddb197b7cca7f212b954cbb984
Martine Ehrlich <martineehrlich@gmail.com>
2018-12-11 10:14:34 -0800

    Bugfix/include pending channels in max channel (#129)
    * add pending channels to getMaxChannel
    
    * add logging to make sure
    
    * add specs
    
    * normalize pendingOpenChannels to avoid two reduce functions, naming cleanup

--- src/engine-actions/get-max-channel.js
+++ src/engine-actions/get-max-channel.js
@@ -1,5 +1,5 @@
 const { Big } = require('../utils')
-const { listChannels } = require('../lnd-actions')
+const { listChannels, listPendingChannels } = require('../lnd-actions')
 
 /**
  * Get local balance of all channels for a specific daemon
@@ -8,15 +8,37 @@ const { listChannels } = require('../lnd-actions')
  * @return {String} res.maxBalance - the max balance in all open channels.
  */
 async function getMaxChannel ({ outbound = true } = {}) {
-  const { channels = [] } = await listChannels({ client: this.client })
-  const balance = outbound ? 'localBalance' : 'remoteBalance'
+  const [
+    { channels = [] } = {},
+    { pendingOpenChannels = [] } = {}
+  ] = await Promise.all([
+    // The response from listChannels consists of channels that may be active or inactive
+    listChannels({ client: this.client }),
+    listPendingChannels({ client: this.client })
+  ])
+  const balanceType = outbound ? 'localBalance' : 'remoteBalance'
 
-  if (channels.length === 0) {
-    this.logger.debug('getMaxChannel: No channels exist')
+  if (!channels.length && !pendingOpenChannels.length) {
+    this.logger.debug('getMaxChannel: No open or pending channels exist')
     return {}
   }
 
-  const maxBalance = channels.reduce((max, channel) => Big(channel[balance]).gt(max) ? Big(channel[balance]) : max, Big(channels[0][balance]))
+  // We need to normalize pendingChannels here because their format is different
+  // than those received from `listChannels`
+  const pendingChannels = pendingOpenChannels.map(chan => chan.channel)
+
+  const allChannels = channels.concat(pendingChannels)
+
+  const maxBalance = allChannels.reduce((max, channel) => {
+    if (Big(channel[balanceType]).gt(max)) {
+      return Big(channel[balanceType])
+    } else {
+      return max
+    }
+  }, Big('0'))
+
+  this.logger.debug(`getMaxChannel: max open channel is: ${maxBalance.toString()}`)
+
   return { maxBalance: maxBalance.toString() }
 }
 
