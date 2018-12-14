d61416a2fa4127963f5a35b97f1c2c60a54bd827
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-12-14 14:59:51 -0800

    updated engine unlocked spec

--- src/engine-actions/is-engine-unlocked.spec.js
+++ src/engine-actions/is-engine-unlocked.spec.js
@@ -6,14 +6,17 @@ const isEngineUnlocked = rewire(path.resolve(__dirname, 'is-engine-unlocked'))
 describe('isEngineUnlocked', () => {
   let engine
   let genSeedStub
+  let isAvailableStub
 
   beforeEach(() => {
     engine = {
       walletUnlocker: sinon.stub()
     }
     genSeedStub = sinon.stub()
+    isAvailableStub = sinon.stub().resolves(true)
 
     isEngineUnlocked.__set__('genSeed', genSeedStub)
+    isEngineUnlocked.__set__('isAvailable', isAvailableStub)
   })
 
   it('makes a call to genSeed to see if lnd is unlocked', async () => {
@@ -27,20 +30,35 @@ describe('isEngineUnlocked', () => {
     expect(isEngineUnlocked.call(engine)).to.eventually.be.rejectedWith(error)
   })
 
-  context('wallet is unlocked', () => {
-    it('returns true if genSeeds is not implemented', async () => {
-      const error = new Error()
-      error.code = 12
-      genSeedStub.throws(error)
-      const res = await isEngineUnlocked.call(engine)
-      expect(res).to.be.eql(true)
-    })
+  it('returns true if genSeeds is not implemented', async () => {
+    const error = new Error()
+    error.code = 12
+    genSeedStub.throws(error)
+    const res = await isEngineUnlocked.call(engine)
+    expect(res).to.be.eql(true)
+  })
 
-    it('returns true if a wallet already exists', async () => {
-      const error = new Error('wallet already exists')
-      genSeedStub.throws(error)
-      const res = await isEngineUnlocked.call(engine)
-      expect(res).to.be.eql(true)
-    })
+  it('returns true if a wallet already exists', async () => {
+    const error = new Error('wallet already exists')
+    genSeedStub.throws(error)
+    const res = await isEngineUnlocked.call(engine)
+    expect(res).to.be.eql(true)
+  })
+
+  it('returns false if a wallet already exists but the wallet is still locked', async () => {
+    const error = new Error('wallet already exists')
+    genSeedStub.throws(error)
+    const lnError = new Error('Unimplemented')
+    lnError.code = 12
+    isAvailableStub.throws(lnError)
+    const res = await isEngineUnlocked.call(engine)
+    expect(res).to.be.eql(false)
+  })
+
+  it('returns true if a wallet already exists and the engine is available', async () => {
+    const error = new Error('wallet already exists')
+    genSeedStub.throws(error)
+    const res = await isEngineUnlocked.call(engine)
+    expect(res).to.be.eql(true)
   })
 })
