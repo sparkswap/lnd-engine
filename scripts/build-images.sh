41987b538e2790b6b2d098c6d96e33c5c8fc3397
Danny Paz <danny@sparkswap.com>
2019-02-06 11:19:04 -0800

    bump version and bump lnd version for port change (#157)
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump version and bump lnd version for port change (#152)
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump lnd version and version bump
    
    FIX - Funding bug for bitcoind/litecoind (#154)
    
    * bump version and bump lnd version for port change
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump version and bump lnd version for port change (#152)
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump lnd version and version bump
    
    funding bug with cron
    
    remove bad comment
    
    * changed text from bitcoind to litecoind
    
    Remove SimNet (#153)
    
    * bump version and bump lnd version for port change
    
    * remove simnet from lnd-engine
    
    * refactor for node for zmq
    
    bump version and bump lnd version for port change (#155)
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump version and bump lnd version for port change (#152)
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump lnd version and version bump
    
    FIX - Funding bug for bitcoind/litecoind (#154)
    
    * bump version and bump lnd version for port change
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump version and bump lnd version for port change (#152)
    
    bump version and bump lnd version for port change (#150)
    
    Improve locked status and timeout (#151)
    
    * add getter for locked status
    
    * add deadline to wallet unlock
    
    * version bump to 0.3.2-beta
    
    bump lnd version and version bump
    
    funding bug with cron
    
    remove bad comment
    
    * changed text from bitcoind to litecoind
    
    Remove SimNet (#153)
    
    * bump version and bump lnd version for port change
    
    * remove simnet from lnd-engine
    
    * refactor for node for zmq
    
    bump version
    
    change tag name of lnd images
    
    add different tagging to lnd-engine
    
    add tagging for lnd images

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -2,9 +2,7 @@
 
 set -e -u
 
-ARG=${1:-remote}
-
-echo "Building images with arg: $ARG"
+echo "Building images for lnd-engine"
 
 # Default branches for each repo
 LND_VERSION='v0.5.3-sparkswap-beta'
@@ -20,13 +18,15 @@ COMMIT_SHA=${LND_VERSION}
 LND_BTC_NODE=bitcoind
 LND_LTC_NODE=litecoind
 
-# NOTE: The names specified with `-t` directly map to the service names in
-# the applicable services docker-compose file
-docker build -t sparkswap_lnd_btc ./docker/lnd --build-arg NODE=$LND_BTC_NODE --build-arg NETWORK=btc --build-arg COMMIT_SHA=$COMMIT_SHA
-docker build -t sparkswap_lnd_ltc ./docker/lnd --build-arg NODE=$LND_LTC_NODE --build-arg NETWORK=ltc --build-arg COMMIT_SHA=$COMMIT_SHA
+ENGINE_VERSION=$(node -pe "require('./package.json').version")
+
+# NOTE: The names specified with `-t` directly map to the our service names in
+# on sparkswap dockerhub
+docker build -t sparkswap/lnd_btc:$ENGINE_VERSION ./docker/lnd --build-arg NODE=$LND_BTC_NODE --build-arg NETWORK=btc --build-arg COMMIT_SHA=$COMMIT_SHA
+docker build -t sparkswap/lnd_ltc:$ENGINE_VERSION ./docker/lnd --build-arg NODE=$LND_LTC_NODE --build-arg NETWORK=ltc --build-arg COMMIT_SHA=$COMMIT_SHA
 
 # Create bitcoind and litecoind images
 BITCOIND_VERSION='0.17.0'
-docker build -t sparkswap_bitcoind ./docker/bitcoind --build-arg VERSION=$BITCOIND_VERSION
+docker build -t sparkswap/bitcoind:$ENGINE_VERSION ./docker/bitcoind --build-arg VERSION=$BITCOIND_VERSION
 LITECOIND_VERSION='0.16.3'
-docker build -t sparkswap_litecoind ./docker/litecoind --build-arg VERSION=$LITECOIND_VERSION
+docker build -t sparkswap/litecoind:$ENGINE_VERSION ./docker/litecoind --build-arg VERSION=$LITECOIND_VERSION
