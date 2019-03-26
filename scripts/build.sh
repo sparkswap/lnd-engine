22436f2b7d2b20d9b19c6a8279d9b6f700c7e85b
Danny Paz <danny@sparkswap.com>
2019-03-26 09:46:17 -0700

    Feature - initiator lnd upgrade (#176)
    * bump minor version of bitcoind
    
    fixed eslint errors for lnd-engine (#163)
    
    * fixed eslint errors for lnd-engine
    
    * turn on eslint
    
    bump minor version of bitcoind (#165)
    
    0.4.0-beta (#164)
    
    Feature/get max channel for address (#166)
    
    * refactor channels for address and add max channel for address
    
    * add specs
    
    * fix docs and add to index
    
    * rename action and add a test
    
    * add getChannelsForRemoteAddress to index
    
    * need context
    
    * do not call function
    
    * add docs
    
    0.4.1-beta (#167)
    
    add script to check for orphaned modules (#168)
    
    added apt-get update to single line so caching issues dont occur in build (#169)
    
    allow users to specify fees for a particular engine (#170)
    
    fixed eslint errors for lnd-engine (#163)
    
    * fixed eslint errors for lnd-engine
    
    * turn on eslint
    
    bump minor version of bitcoind (#165)
    
    0.4.0-beta (#164)
    
    Feature/get max channel for address (#166)
    
    * refactor channels for address and add max channel for address
    
    * add specs
    
    * fix docs and add to index
    
    * rename action and add a test
    
    * add getChannelsForRemoteAddress to index
    
    * need context
    
    * do not call function
    
    * add docs
    
    0.4.1-beta (#167)
    
    add script to check for orphaned modules (#168)
    
    added apt-get update to single line so caching issues dont occur in build (#169)
    
    allow users to specify fees for a particular engine
    
    refactor of env names
    
    rename vars
    
    0.4.2-beta (#171)
    
    Use a block buffer in the second leg of the swap (#172)
    
    * include a block buffer for the second leg of the swap to prevent newly mined blocks from causing a rejection
    
    * fmt
    
    * package lock
    
    remove build-images and consolidate build
    
    * remove the need to npm install when building lnd-engine
    
    * add correct lnd version for build script
    
    remove need for npm install in build script for users
    
    update proto file for v0.5.4
    
    revert lock change

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,12 +16,15 @@ echo ""
 echo "It's time to BUILD! All resistance is futile."
 echo ""
 
+LND_VERSION='v0.5.4-sparkswap-beta'
+
 # Downloads an LND proto file from the sparkswap/lnd fork
-LND_PROTO_URL=${LND_PROTO_URL:-https://raw.githubusercontent.com/sparkswap/lnd/k%23epic/cross-chain-preimage/lnrpc/rpc.proto}
+LND_PROTO_URL="https://raw.githubusercontent.com/sparkswap/lnd/${LND_VERSION}/lnrpc/rpc.proto"
 
 rm -rf ./proto
 mkdir -p ./proto
 
+echo "Downloading lnd proto file for version: ${LND_VERSION}"
 curl -o ./proto/lnd-rpc.proto $LND_PROTO_URL
 
 # Prepares the downloaded lnd-rpc proto file (installation steps tell you to remove this line)
@@ -29,9 +32,6 @@ curl -o ./proto/lnd-rpc.proto $LND_PROTO_URL
 sed 's|^import \"google/api/annotations.proto\";||' ./proto/lnd-rpc.proto > /tmp/file.$$ && mv /tmp/file.$$ ./proto/lnd-rpc.proto
 
 echo "Building images for lnd-engine"
-
-# Default branches for each repo
-LND_VERSION='v0.5.3-sparkswap-beta'
 
 # We add a COMMIT_SHA argument to the lnd dockerfile to trigger cache-invalidation
 # when get git clone the sparkswap/lnd repo. Without it, docker would continue
