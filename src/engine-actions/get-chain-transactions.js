0af0dd515cd98ab1d65b5300085e0b179b486874
Danny Paz <danny@sparkswap.com>
2019-04-17 15:38:45 -0700

    Feature - getChainTransactions (#198)
    * added response for open channels
    
    revert channel change and add transactions endpoint
    
    remove date conversion for timestamp
    
    added tests for get chain transactions
    
    fix transactions
    
    edits to transactions
    
    added commentary for edge case
    
    find all tx
    
    add logs
    
    add fees for closed channel
    
    added more comments for chain transactions
    
    refactor code a little bit to be more readable
    
    add a pending type for a blockheight of 0
    
    add in info for pending open channels
    
    fix call to pending channels
    
    fix reduce
    
    refactor code to be more readable
    
    changed fee to map
    
    Feature - Add total reserved channel balance (#197)
    
    * add getCommitFees engine action
    
    * update method name
    
    * filter channels for initiators instead of using ternary
    
    * add tests
    
    * update comment
    
    * remove guard and update logging
    
    * update naming
    
    * adress PR feedback
    
    Update mocha to the latest version 🚀 (#199)
    
    * chore(package): update mocha to version 6.1.3
    
    * chore(package): update lockfile package-lock.json
    
    0.5.1-beta (#200)
    
    added tests for funding tx
    
    fix logs for balances if channels are empty (#201)
    
    Update eslint-plugin-import to the latest version 🚀 (#202)
    
    * chore(package): update eslint-plugin-import to version 2.17.0
    
    * chore(package): update lockfile package-lock.json
    
    Update @grpc/proto-loader to the latest version 🚀 (#196)
    
    * fix(package): update @grpc/proto-loader to version 0.5.0
    
    * chore(package): update lockfile package-lock.json
    
    added types
    
    change from blocknumber to blockheight
    
    changed transaction to transaction hash
    
    refactor for status codes
    
    fix channel type
    
    refactor of chain transactions
    
    revisit comment on fees
    
    added initiator to transactions
    
    added lnd change for initiator and added logic for closed channels
    
    Revert "revisit comment on fees"
    
    This reverts commit 09ed224b8e4d0af1bc388ce1a0f28d7db80c6cfc.
    
    change comment
    
    Revert "added lnd change for initiator and added logic for closed channels"
    
    This reverts commit b7a9efe8ebbc5dd72db2dfba5e045b0cbf129238.
    
    Revert "added initiator to transactions"
    
    This reverts commit 2f2f7422242af605a305621eeeedf2955427ecd8.
    
    added todo for initiator field
    
    * fix comment for initiator
    
    * remove underscore
    
    * fix return value for get transactions

---
+++ src/engine-actions/get-chain-transactions.js
@@ -0,0 +1,191 @@
+const {
+  getTransactions,
+  listClosedChannels,
+  listPendingChannels,
+  listChannels
+} = require('../lnd-actions')
+const { Big } = require('../utils')
+
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
+const TRANSACTION_TYPES = Object.freeze({
+  CHANNEL_OPEN: 'CHANNEL_OPEN',
+  CHANNEL_CLOSE: 'CHANNEL_CLOSE',
+  DEPOSIT: 'DEPOSIT',
+  WITHDRAW: 'WITHDRAW',
+  UNKNOWN: 'UNKNOWN'
+})
+
+/**
+ * Gets all opening and closing transaction ids from a list of channels.
+ * @param {Object} channels
+ * @returns {Set} channelTxIds
+ */
+async function getTxIdsForChannels (channels) {
+  return channels.reduce((channelTxIds, { closingTxHash, channelPoint }) => {
+    const [ openingTxHash ] = channelPoint.split(':') // eslint-disable-line
+
+    // The closingTxHash will be missing from open or pending channels
+    if (closingTxHash) {
+      channelTxIds.add(closingTxHash)
+    }
+
+    channelTxIds.add(openingTxHash)
+    return channelTxIds
+  }, new Set())
+}
+
+/**
+ * Gets closing transaction fees from closed channel objects
+ * @param {Object} closedChannels
+ * @returns {Set} closedChannelFees
+ */
+async function getFeesForClosedChannels (closedChannels) {
+  return closedChannels.reduce((closedChannelFees, { closingTxHash, capacity, settledBalance }) => {
+    // A closed channel would not have a closingTxHash if the we are looking at a
+    // pending closed channel
+    if (closingTxHash) {
+      // We can calculate the fees that were incurred when closing a channel by taking
+      // the capacity minus the settled balance. When we call LND to get transactions,
+      // the fee for a channel closing will not be present so we need to calculate it
+      // here
+      closedChannelFees.set(closingTxHash, Big(capacity).minus(settledBalance).toString())
+    }
+    return closedChannelFees
+  }, new Map())
+}
+
+/**
+ * @param {string} amount
+ * @param {boolean} isChannel
+ * @param {boolean} isClosedChannel
+ * @returns {TRANSACTION_TYPES} type
+ */
+function determineTransactionType (amount, isChannel, isClosedChannel) {
+  // A channel close will have a positive balance as we are putting currency back
+  // into our wallet from the payment channel and will be listed in LNDs closed channels
+  if (Big(amount).gt(0) && isClosedChannel) {
+    return TRANSACTION_TYPES.CHANNEL_CLOSE
+  }
+
+  // A channel open will have a negative balance to show for funds that are being
+  // put into the payment channels and will be listed in LNDs channel db
+  if (Big(amount).lt(0) && isChannel) {
+    return TRANSACTION_TYPES.CHANNEL_OPEN
+  }
+
+  // Since we've already checked `gt` balances w/ closed channels, we can assume
+  // that any credit will be an on-chain deposit
+  if (Big(amount).gt(0)) {
+    return TRANSACTION_TYPES.DEPOSIT
+  }
+
+  // Since we've already checked `gt` balances w/ closed channels, we can assume
+  // that any debit will be an on-chain withdraw
+  if (Big(amount).lt(0)) {
+    return TRANSACTION_TYPES.WITHDRAW
+  }
+
+  // If we do not hit any of the types above, then we can return unknown.
+  return TRANSACTION_TYPES.UNKNOWN
+}
+
+/**
+ * Gets all on-chain transactions for the engine
+ * @returns {Array<Object>} res
+ */
+async function getChainTransactions () {
+  const { transactions } = await getTransactions({ client: this.client })
+
+  if (!Array.isArray(transactions) || !transactions.length) {
+    this.logger.debug('No transactions exist on the engine')
+    return []
+  }
+
+  // Get all channels from the engine (rough)
+  const [
+    { channels: rawChannels },
+    { channels: rawClosedChannels },
+    {
+      pendingOpenChannels: rawPendingOpenChannels,
+      pendingClosingChannels: rawPendingClosingChannels,
+      pendingForceClosingChannels: rawPendingForceClosingChannels,
+      waitingCloseChannels: rawWaitingCloseChannels
+    }
+  ] = await Promise.all([
+    listChannels({ client: this.client }),
+    listClosedChannels({ client: this.client }),
+    listPendingChannels({ client: this.client })
+  ])
+
+  const openChannels = [
+    ...rawChannels,
+    // Pending channels have a different structure than our typical Channel object
+    // so we need to grab the channel off of the raw payload
+    ...rawPendingOpenChannels.map(chan => chan.channel)
+  ]
+
+  const closedChannels = [
+    ...rawClosedChannels,
+    // Pending channels have a different structure than our typical Channel object
+    // so we need to grab the channel off of the raw payload
+    ...rawPendingClosingChannels.map(chan => chan.channel),
+    ...rawPendingForceClosingChannels.map(chan => chan.channel),
+    ...rawWaitingCloseChannels.map(chan => chan.channel)
+  ]
+
+  const pendingChannels = [
+    ...rawPendingOpenChannels.map(chan => chan.channel),
+    ...rawPendingClosingChannels.map(chan => chan.channel),
+    ...rawPendingForceClosingChannels.map(chan => chan.channel),
+    ...rawWaitingCloseChannels.map(chan => chan.channel)
+  ]
+
+  const [
+    closedChannelTxIds,
+    closedChannelFees,
+    openChannelTxIds,
+    pendingChannelTxIds
+  ] = await Promise.all([
+    getTxIdsForChannels(closedChannels),
+    getFeesForClosedChannels(rawClosedChannels),
+    getTxIdsForChannels(openChannels),
+    getTxIdsForChannels(pendingChannels)
+  ])
+
+  return transactions.map(({ txHash, amount, blockHeight, timeStamp, totalFees }) => {
+    const isClosedChannel = closedChannelTxIds.has(txHash)
+    const isChannel = (openChannelTxIds.has(txHash) || closedChannelTxIds.has(txHash))
+    const type = determineTransactionType(amount, isChannel, isClosedChannel)
+
+    // Determine is the transaction is pending. Having a blockHeight of 0 means
+    // that the transaction has not yet been placed into a block (no confirmations)
+    const isPending = (Big(blockHeight).eq(0) || pendingChannelTxIds.has(txHash))
+
+    // `totalFees` will be zero if this is a CHANNEL_CLOSE transaction type
+    let fees = Big(totalFees)
+
+    // If we've identified the type of transaction as channel close then `totalFees`
+    // will be 0. We calculate the fee based on information from the closing transaction
+    // but this is only available if the transaction has hit the blockchain
+    if (type === TRANSACTION_TYPES.CHANNEL_CLOSE && !isPending) {
+      // TODO: Only re-assign `fees` if we are the initiator of the channel.
+      fees = Big(closedChannelFees.get(txHash))
+    }
+
+    return {
+      type,
+      amount: Big(amount).div(this.quantumsPerCommon).toFixed(8),
+      transactionHash: txHash,
+      blockHeight,
+      timestamp: timeStamp,
+      fees: fees.div(this.quantumsPerCommon).toFixed(8),
+      pending: isPending
+    }
+  })
+}
+
+module.exports = getChainTransactions
---
+++ src/engine-actions/get-chain-transactions.spec.js
@@ -0,0 +1,272 @@
+const path = require('path')
+const {
+  expect,
+  rewire,
+  sinon
+} = require('test/test-helper')
+
+const { Big } = require('../utils')
+
+const getChainTransactions = rewire(path.resolve(__dirname, 'get-chain-transactions'))
+
+describe('getChainTransactions', () => {
+  describe('getTxIdsForChannels', () => {
+    let channels
+    let getTxIdsForChannels = getChainTransactions.__get__('getTxIdsForChannels')
+
+    beforeEach(() => {
+      channels = [
+        { channelPoint: '2345:1' },
+        { channelPoint: '1234:0', closingTxHash: 'closingtxhash' }
+      ]
+    })
+
+    it('returns transaction ids for a list of channels', async () => {
+      const res = await getTxIdsForChannels([channels[0]])
+      expect(res.has('2345')).to.be.true()
+    })
+
+    it('includes closing transactions if they exist', async () => {
+      const res = await getTxIdsForChannels(channels)
+      expect(res.has('1234')).to.be.true()
+      expect(res.has('2345')).to.be.true()
+      expect(res.has('closingtxhash')).to.be.true()
+    })
+  })
+
+  describe('getFeesForClosedChannels', () => {
+    let closedChannels
+    let getFeesForClosedChannels = getChainTransactions.__get__('getFeesForClosedChannels')
+
+    beforeEach(() => {
+      closedChannels = [{
+        closingTxHash: 'closingtxhash',
+        capacity: '10000',
+        settledBalance: '9000'
+      }]
+    })
+
+    it('returns an empty map when no closed channels exist', async () => {
+      const res = await getFeesForClosedChannels([])
+      expect(res).to.be.empty()
+    })
+
+    it('returns a fee for a given txhash', async () => {
+      const expectedFee = '1000'
+      const res = await getFeesForClosedChannels(closedChannels)
+      expect(res.get(closedChannels[0].closingTxHash)).to.be.eql(expectedFee)
+    })
+  })
+
+  describe('determineTransactionType', () => {
+    let TRANSACTION_TYPES
+    let determineTransactionType
+    let amount
+    let isChannel
+    let isClosedChannel
+
+    beforeEach(() => {
+      amount = '100000000'
+      isChannel = false
+      isClosedChannel = false
+
+      determineTransactionType = getChainTransactions.__get__('determineTransactionType')
+      TRANSACTION_TYPES = getChainTransactions.__get__('TRANSACTION_TYPES')
+    })
+
+    it('returns CHANNEL_CLOSE if the channel was found in the closed channels', () => {
+      amount = '1'
+      isClosedChannel = true
+      expect(determineTransactionType(amount, isChannel, isClosedChannel)).to.be.eql(TRANSACTION_TYPES.CHANNEL_CLOSE)
+    })
+
+    it('returns CHANNEL_OPEN if our transaction amount is negative and our txid is found in our channels on the engine', () => {
+      amount = '-0.12'
+      isChannel = true
+      expect(determineTransactionType(amount, isChannel, isClosedChannel)).to.be.eql(TRANSACTION_TYPES.CHANNEL_OPEN)
+    })
+
+    it('returns DEPOSIT if the transaction is not in a channel and has a positive amount', () => {
+      amount = '1'
+      expect(determineTransactionType(amount, isChannel, isClosedChannel)).to.be.eql(TRANSACTION_TYPES.DEPOSIT)
+    })
+
+    it('returns WITHDRAW if the transaction is not in a channel and has a negative amount', () => {
+      amount = '-0.12'
+      expect(determineTransactionType(amount, isChannel, isClosedChannel)).to.be.eql(TRANSACTION_TYPES.WITHDRAW)
+    })
+
+    it('returns UNKNOWN for an unknown transaction', () => {
+      amount = '0'
+      expect(determineTransactionType(amount, isChannel, isClosedChannel)).to.be.eql(TRANSACTION_TYPES.UNKNOWN)
+    })
+  })
+
+  describe('getChainTransactions', () => {
+    let getTransactionsStub
+    let engine
+    let transactions
+    let listChannelsStub
+    let listClosedChannelsStub
+    let listPendingChannelsStub
+    let openChannels
+    let closedChannels
+    let pendingChannels
+
+    beforeEach(() => {
+      transactions = [
+        // Deposit transaction
+        { txHash: 'deposit', amount: '201000', blockHeight: '123', timeStamp: '1234', totalFees: '0' },
+        // Channel open transaction
+        { txHash: 'channelopen', amount: '-11000', blockHeight: '1234', timeStamp: '1234', totalFees: '1000' },
+        { txHash: 'pendingchannelopen', amount: '-11000', blockHeight: '0', timeStamp: '1234', totalFees: '0' },
+        // Channel close transactions
+        { txHash: 'channelclose', amount: '9000', blockHeight: '12345', timeStamp: '1234', totalFees: '0' },
+        { txHash: 'pendingchannelclose', amount: '9000', blockHeight: '12345', timeStamp: '1234', totalFees: '0' },
+        { txHash: 'pendingforceclose', amount: '9000', blockHeight: '12345', timeStamp: '1234', totalFees: '0' },
+        // Withdraw transaction
+        { txHash: 'withdraw', amount: '-20000', blockHeight: '1234', timeStamp: '1234', totalFees: '1000' },
+        // Pending tx
+        { txHash: 'pendingwaitingclose', amount: '9000', blockHeight: '0', timeStamp: '1234', totalFees: '0' },
+        // Unknown tx
+        { txHash: 'unknown', amount: '0', blockHeight: '123455', timeStamp: '1234', totalFees: '1000' }
+      ]
+
+      engine = {
+        logger: {
+          debug: sinon.stub()
+        },
+        client: sinon.stub(),
+        quantumsPerCommon: 1
+      }
+
+      openChannels = {
+        channels: [
+          { channelPoint: 'channelopen:0' }
+        ]
+      }
+
+      closedChannels = {
+        channels: [
+          { channelPoint: 'channelclose:0', closingTxHash: 'channelclose', capacity: '10000', settledBalance: '9000' }
+        ]
+      }
+
+      pendingChannels = {
+        pendingOpenChannels: [
+          { channel: { channelPoint: 'pendingchannelopen:0' } }
+        ],
+        pendingClosingChannels: [
+          { channel: { channelPoint: 'pendingchannelclose:0' } }
+        ],
+        pendingForceClosingChannels: [
+          { channel: { channelPoint: 'pendingforceclose:0' } }
+        ],
+        waitingCloseChannels: [
+          { channel: { channelPoint: 'pendingwaitingclose:0' } }
+        ]
+      }
+
+      // LND stubs
+      listChannelsStub = sinon.stub().resolves(openChannels)
+      listClosedChannelsStub = sinon.stub().resolves(closedChannels)
+      listPendingChannelsStub = sinon.stub().resolves(pendingChannels)
+
+      getChainTransactions.__set__('listChannels', listChannelsStub)
+      getChainTransactions.__set__('listClosedChannels', listClosedChannelsStub)
+      getChainTransactions.__set__('listPendingChannels', listPendingChannelsStub)
+
+      getTransactionsStub = sinon.stub().resolves({ transactions })
+
+      getChainTransactions.__set__('getTransactions', getTransactionsStub)
+    })
+
+    it('returns an empty array if no transactions are available', () => {
+      getTransactionsStub.resolves([])
+      expect(getChainTransactions.call(engine)).to.be.empty()
+    })
+
+    it('returns a list of chain transactions', async () => {
+      const res = await getChainTransactions.call(engine)
+      expect(Array.isArray(res)).to.be.true()
+    })
+
+    describe('returned transactions', () => {
+      const TYPES = getChainTransactions.__get__('TRANSACTION_TYPES')
+
+      it('returns a deposit transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[0]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.DEPOSIT)
+        expect(res[0].pending).to.be.false()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[0].totalFees).toString())
+      })
+
+      it('returns a channel open transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[1]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_OPEN)
+        expect(res[0].pending).to.be.false()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[1].totalFees).toString())
+      })
+
+      it('returns a pending channel open transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[2]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_OPEN)
+        expect(res[0].pending).to.be.true()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[2].totalFees).toString())
+      })
+
+      it('returns a channel close transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[3]] })
+        const res = await getChainTransactions.call(engine)
+        const expectedFee = Big(closedChannels.channels[0].capacity).minus(closedChannels.channels[0].settledBalance).toString()
+
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_CLOSE)
+        expect(res[0].pending).to.be.false()
+        expect(Big(res[0].fees).toString()).to.eql(expectedFee)
+      })
+
+      it('returns a pending channel close transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[4]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_CLOSE)
+        expect(res[0].pending).to.be.true()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[4].totalFees).toString())
+      })
+
+      it('returns a pending force close transactions', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[5]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_CLOSE)
+        expect(res[0].pending).to.be.true()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[5].totalFees).toString())
+      })
+
+      it('returns a withdraw transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[6]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.WITHDRAW)
+        expect(res[0].pending).to.be.false()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[6].totalFees).toString())
+      })
+
+      it('returns a pending waiting close transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[7]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.CHANNEL_CLOSE)
+        expect(res[0].pending).to.be.true()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[7].totalFees).toString())
+      })
+
+      it('returns an unknown transaction', async () => {
+        getTransactionsStub.resolves({ transactions: [transactions[8]] })
+        const res = await getChainTransactions.call(engine)
+        expect(res[0].type).to.be.eql(TYPES.UNKNOWN)
+        expect(res[0].pending).to.be.false()
+        expect(Big(res[0].fees).toString()).to.eql(Big(transactions[8].totalFees).toString())
+      })
+    })
+  })
+})
