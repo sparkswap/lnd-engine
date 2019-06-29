f3629ac613322d3ba2bf1d8a3cb3b72c41e567e4
Chris Clark <clark800@users.noreply.github.com>
2019-06-28 18:52:05 -0700

    Add expiry parameters to prepareSwap and make it idempotent (#229)

--- src/engine-actions/prepare-swap.js
+++ src/engine-actions/prepare-swap.js
@@ -1,13 +1,4 @@
-const { addHoldInvoice } = require('../lnd-actions')
-
-/**
- * default expiry for swaps is 3600 seconds - 1 hour.
- * @todo Should this be changed to something shorter or be configurable
- * @todo Does this need to be dynamic based on the swap hash invoice
- * @constant
- * @type {String}
- */
-const SWAP_EXPIRY = '3600'
+const { addHoldInvoice, lookupInvoice } = require('../lnd-actions')
 
 /**
  * The memo prefix allows us to easily find SparkSwap-related invoices
@@ -17,29 +8,51 @@ const SWAP_EXPIRY = '3600'
  * @type {String}
  * @default
  */
-const MEMO_PREFIX = 'sparkswap-swap-pivot:'
+const MEMO_PREFIX = 'sparkswap-swap-pivot'
 
 /**
  * Prepares for a swap in which this node is the counterparty to the intiating node
  *
- * @param {string} orderId - Order for which the swap is being executed
- * @param {string} swapHash        - swap hash that will be associated with the swap
- * @param {string} value   - Int64 string of the value of inbound currency
- * @returns {string} swapHash      Hash that will be translated
+ * @param {string} swapHash - base64 hash that will be associated with the swap
+ * @param {string} value - Int64 string of the value of inbound currency
+ * @param {Date} expiryTime - absolute payment request expiry time
+ * @param {number} cltvExpiry - delta to use for the time-lock of the CLTV
+ *                              extended to the final hop (in seconds)
+ * @returns {string} paymentRequest - lightning invoice for a payment
  */
-async function prepareSwap (orderId, swapHash, value) {
+async function prepareSwap (swapHash, value, expiryTime, cltvExpiry) {
   this.logger.info(`Preparing swap for ${swapHash}`, { value })
+  // round up on number of blocks since a transaction that occurs right before
+  // the expiration time will get processed in the following block
+  const cltvExpiryBlocks = Math.ceil(cltvExpiry / this.secondsPerBlock)
+  // make prepareSwap idempotent by returning the existing invoice if one exists
+  try {
+    const {
+      paymentRequest,
+      value: invoiceValue,
+      cltvExpiry: invoiceCLTVExpiry
+    } = await lookupInvoice(swapHash, { client: this.client })
 
-  const params = {
-    memo: `${MEMO_PREFIX}${orderId}`,
-    value,
-    expiry: SWAP_EXPIRY,
-    hash: swapHash
+    if (invoiceValue !== value || invoiceCLTVExpiry !== cltvExpiryBlocks) {
+      // if the invoice doesn't match, we log an error and  prepare a new invoice
+      const message = `Invoice found for hash ${swapHash} but parameters are wrong`
+      this.logger.error(message)
+      throw new Error(message)
+    }
+    return paymentRequest
+  } catch (e) {
+    const expiry = Math.floor((expiryTime - new Date()) / 1000)
+    const params = {
+      memo: `${MEMO_PREFIX}`,
+      hash: swapHash,
+      value,
+      expiry,
+      cltvExpiry: cltvExpiryBlocks
+    }
+
+    const { paymentRequest } = await addHoldInvoice(params, { client: this.client })
+    return paymentRequest
   }
-
-  const { paymentRequest } = await addHoldInvoice(params, { client: this.client })
-
-  return paymentRequest
 }
 
 module.exports = prepareSwap
--- src/engine-actions/prepare-swap.spec.js
+++ src/engine-actions/prepare-swap.spec.js
@@ -4,37 +4,68 @@ const { expect, rewire, sinon } = require('test/test-helper')
 const prepareSwap = rewire(path.resolve(__dirname, 'prepare-swap'))
 
 describe('prepareSwap', () => {
-  let orderId
   let value
   let addHoldInvoiceStub
-  let clientStub
+  let lookupInvoiceStub
   let invoiceResponse
   let paymentRequest
+  let existingPaymentRequest
   let swapHash
-  let res
+  let expiryTime
+  let cltvExpiry
+
+  const client = sinon.stub()
+  const engine = {
+    client,
+    secondsPerBlock: 600,
+    logger: {
+      info: sinon.stub(),
+      error: sinon.stub(),
+      debug: sinon.stub()
+    }
+  }
 
   beforeEach(() => {
-    orderId = '928uq9afds8as9df_fasdfj'
     value = '100'
     paymentRequest = '1234'
+    existingPaymentRequest = '12345'
     swapHash = '1234'
+    // add 10ms to 60s so flooring to the nearest second in prepareSwap gives 60
+    expiryTime = new Date((new Date()).getTime() + 60010)
+    cltvExpiry = 3600
     invoiceResponse = { paymentRequest }
     addHoldInvoiceStub = sinon.stub().resolves(invoiceResponse)
-    clientStub = sinon.stub()
 
+    lookupInvoiceStub = sinon.stub().throws({})
+    prepareSwap.__set__('lookupInvoice', lookupInvoiceStub)
     prepareSwap.__set__('addHoldInvoice', addHoldInvoiceStub)
-    prepareSwap.__set__('client', clientStub)
+    prepareSwap.__set__('client', client)
   })
 
-  beforeEach(async () => {
-    res = await prepareSwap(orderId, swapHash, value)
+  it('adds an invoice through lnd', async () => {
+    await prepareSwap.call(engine, swapHash, value, expiryTime, cltvExpiry)
+    expect(addHoldInvoiceStub).to.have.been.calledWith({
+      memo: 'sparkswap-swap-pivot',
+      hash: swapHash,
+      value,
+      expiry: 60,
+      cltvExpiry: 6
+    }, sinon.match({ client }))
   })
 
-  it('adds an invoice through lnd', () => {
-    expect(addHoldInvoiceStub).to.have.been.calledWith({ memo: `sparkswap-swap-pivot:${orderId}`, expiry: '3600', value, hash: swapHash }, sinon.match({ client: clientStub }))
+  it('is idempotent', async () => {
+    const lookupInvoiceStub2 = sinon.stub().resolves({
+      paymentRequest: existingPaymentRequest,
+      cltvExpiry: 6,
+      value
+    })
+    prepareSwap.__set__('lookupInvoice', lookupInvoiceStub2)
+    const res = await prepareSwap.call(engine, swapHash, value, expiryTime, cltvExpiry)
+    expect(res).to.be.eql(existingPaymentRequest)
   })
 
-  it('returns an invoice hash hash', () => {
+  it('returns an invoice hash', async () => {
+    const res = await prepareSwap.call(engine, swapHash, value, expiryTime, cltvExpiry)
     expect(res).to.be.eql(paymentRequest)
   })
 })
